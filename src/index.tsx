e7efb487a34c86adfe4a8c3a542ee301550ea776
Sanka Sanjeewa <sanka.s@sms.fidenz.com>
2023-01-22 18:50:46 +0530

    configuring twin

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { GlobalStyles } from "twin.macro";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
 );
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  theme: {
+    colors: {
+      ...colors,
+      white: "#FFFFFF",
+      black: "000000",
+      active: "36CE00",
+    },
+  },
+  plugins: [],
+};
